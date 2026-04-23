c73f6b8d8786fafddf93c5ce18ff03590f256434
Alaeddin NASSANI <alaeddin@ahlab.org>
2026-04-23 11:32:25 +0900

    health

--- Assets/Scripts/Wall.cs
+++ Assets/Scripts/Wall.cs
@@ -7,6 +7,9 @@ public class Wall : MonoBehaviour
     public GameObject block;
     public int width = 10;
     public int height = 4;
+
+    private int numberOfBlocks = 0;
+
     void Start()
     {
         for (int y=0; y<height; ++y)
@@ -15,7 +18,16 @@ public class Wall : MonoBehaviour
             {
                 Vector3 offset = new Vector3(x, y, 0);
                 Instantiate(block, transform.position + offset, Quaternion.identity);
+                numberOfBlocks++;
             }
         }        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("space pressed");
+        }
     }
 }
--- Assets/Scripts/WreckOnCollision.cs
+++ Assets/Scripts/WreckOnCollision.cs
@@ -6,10 +6,18 @@ public class WreckOnCollision : MonoBehaviour
 {
     public GameObject wreckedVersion;
 
+    public int health = 100;
+
     // Update is called once per frame
     void OnCollisionEnter()
     {
-        Destroy(gameObject);
-        Instantiate(wreckedVersion,transform.position,transform.rotation);
+        health -=50;
+
+        if (health == 0)
+        {
+            Destroy(gameObject);
+            Instantiate(wreckedVersion,transform.position,transform.rotation);            
+        }
+
     }
 }
